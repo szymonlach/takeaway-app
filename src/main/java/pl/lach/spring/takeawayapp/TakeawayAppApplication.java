d5aa1fa04645c66ac7b9fa7845863acf205fdfb7
Szymon Lach <szymom.sp1@gmail.com>
2019-03-23 14:23:57 +0100

    Project initialization

---
+++ src/main/java/pl/lach/spring/takeawayapp/TakeawayAppApplication.java
@@ -0,0 +1,13 @@
+package pl.lach.spring.takeawayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeawayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeawayAppApplication.class, args);
+    }
+
+}
